4f6083a62b36700cf90b2514c0941a7bec43b987
rich smith <rich__smith@hotmail.com>
2014-01-29 08:52:23 +1100

    added files

---
+++ ToDo/Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Web;
+using Ninject;
+using Ninject.Extensions.Conventions;
+namespace ToDo.Bootstrapper
+{
+    public class Bootstrapper
+    {
+        public IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Bind(x => x
+                   .FromThisAssembly()
+                       .SelectAllClasses()
+                   .BindDefaultInterface()
+                   .Configure(b => b.InScope(ctx => HttpContext.Current)
+              ));
+
+            kernel.Bind(x => x
+                  .FromAssemblyContaining<Core.Services.ToDoService>()
+                      .SelectAllClasses()
+                  .BindDefaultInterface()
+                  .Configure(b => b.InScope(ctx => HttpContext.Current)
+             ));
+
+            //load all ninjectmodules in this assembly!
+            kernel.Load(Assembly.GetExecutingAssembly().Location);
+
+            return kernel;
+        }
+    }
+}
---
+++ ToDo/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Services;
+
+namespace ToDo.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IToDoService _toDoService;
+
+        public TestController(IToDoService toDoService)
+        {
+            _toDoService = toDoService;
+            
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View("");
+        }
+    }
+    public class HomeController : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+              ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
