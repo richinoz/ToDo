91cbe0ab8870d4b181b0b12c169a912af1bc9f7a
Richard Smith <richloveswork@gmail.com>
2014-01-29 11:17:57 +1100

    created test controller

--- ToDo/Controllers/HomeController.cs
+++ ToDo/Controllers/HomeController.cs
@@ -3,26 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Core.Services;
 
 namespace ToDo.Controllers
 {
-    public class TestController : Controller
-    {
-        private readonly IToDoService _toDoService;
-
-        public TestController(IToDoService toDoService)
-        {
-            _toDoService = toDoService;
-            
-        }
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View("");
-        }
-    }
     public class HomeController : Controller
     {
         
