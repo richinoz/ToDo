753dae6c03e132f5ebdba293327ad7f4a7145146
Richard Smith <richloveswork@gmail.com>
2014-01-30 14:42:30 +1100

    removed nuget packages

---
+++ src/ToDo/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Ninject;
+using Ninject.Web.Common;
+
+namespace ToDo
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : NinjectHttpApplication
+    {
+        protected override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var bootstrapper = new Bootstrapper.Bootstrapper();
+            return bootstrapper.CreateKernel();
+        }
+    }
+}
